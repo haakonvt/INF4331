0f0b2672adafa0812afe61ed03bc221313319a65
haakonvt <haakonvt@gmail.com>
2015-09-07 17:29:40 +0200

    Fixed size-input on compress

--- week1/compress.sh
+++ week1/compress.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ "$2" != "" ] || [ "$1" = "" ]; then
-   echo "Wrong number of arguments, only need one: path to directory"
+if [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
+   echo "Please input the path as first argument to the directory you want to look for files to compress. I.e. ~/../MyFolder/"
    exit 1
 fi
-if [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
-   echo "Please input the path as first argument to the directory you want to look for files to compress. I.e. ~/../MyFolder/"<&2
+
+if [ "$#" != "2" ]; then
+   echo "Wrong number of arguments, only need two:"
+   echo "1) path to directory"
+   echo "2) file minimum size"
+   exit 1
 else
-   gzip `find "$1" -size +200k`
+   gzip `find "$1" -size +"$2"k`
    exit 1
 fi
