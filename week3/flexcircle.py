3074f14e9ec47b5bfece1e596369eafeacbe3895
haakonvt <haakonvt@gmail.com>
2015-09-16 01:23:33 +0200

    Oblig 3 done

--- week3/flexcircle.py
+++ week3/flexcircle.py
@@ -1,27 +1,44 @@
 class FlexCircle(object):
-    "Define a circle with only a given radius"
+    """Define a circle with only a given radius.
+    If then area or perimeter is changed, the other value will
+    be updated with the correct value"""
 
     def __init__(self,radius):
         "Initialise and save radius for the current object"
-        self.pi        = 3.14159265359
-        self.radius    = radius
+        if radius < 0:
+            import sys
+            print "Radius can't be negative, try again!"
+            sys.exit()
+        else:
+            self.pi        = 3.14159265359
+            self.radius    = radius
 
     @property
     def area(self):
 		return self.radius**2*self.pi
     @area.setter
     def area(self, area):
-		self.radius     = (area/self.pi)**(0.5)
-		self._perimeter = area*2.0/self.radius
+        if area < 0:
+            import sys
+            print "Area can't be negative, try again!"
+            sys.exit()
+        else:
+		    self.radius     = (area/self.pi)**(0.5)
+		    self._perimeter = area*2.0/self.radius
 
     @property
     def perimeter(self):
         return self.radius*2*self.pi
 
     @perimeter.setter
-    def perimeter(self, new_perimeter):
-        self.radius = new_perimeter/(2.0*self.pi)
-        self._area  = self.pi*self.radius*self.radius
+    def perimeter(self, perimeter):
+        if perimeter < 0:
+            import sys
+            print "Perimeter can't be negative, try again!"
+            sys.exit()
+        else:
+            self.radius = perimeter/(2.0*self.pi)
+            self._area  = self.pi*self.radius*self.radius
 
 
 if __name__ == "__main__":
@@ -31,6 +48,6 @@ if __name__ == "__main__":
     print "Now setting perimeter to 50.."
     c.perimeter = 50
     print "Area, perimeter = ", c.area, ",", c.perimeter
-    print "Now setting area to 24.."
-    c.area = 24
+    print "Now setting area to -24.."
+    c.area = -24
     print "Area, perimeter = ", c.area, ",", c.perimeter
--- week3/ranking.py
+++ week3/ranking.py
@@ -1,23 +1,22 @@
-
 filename 	 = "data.log"
 log_file 	 = open(filename, 'r')
 list_of_file = log_file.readlines()
 
+cpu_dict = {} # Dictionary to store test name and cpu-times
+
 for line in list_of_file:
 	if "CPU" in line:
-		
+		word_list = line.split()
+		current_test_name  = word_list[1]
+		current_time_taken = float(word_list[3])
+		if current_test_name not in cpu_dict:
+			cpu_dict[current_test_name] = []
+		cpu_dict[current_test_name].append(current_time_taken)
 
-
-
-
-
-"""
-for filename in sys.argv[1:]:
-	word_counter  = 0
-	crnt_file     = open(filename,'r')    # Opens file for reading
-	list_of_file  = crnt_file.readlines() # List of all lines
-	for line in list_of_file:			  # Loop over all lines
-		line_list = line.split()		  # Split all words
-		word_counter += len(line_list)	  # Count all words
-	print "%s: %i" %(filename, word_counter)
-"""
+for key in cpu_dict:
+	cpu_times = map(float, cpu_dict[key])
+	avg = sum(cpu_times)/len(cpu_times)
+	print "Test name: %s" %key
+	print "CPU time: %.1f s (min)"   % min(cpu_times)
+	print "          %.1f s (avg)"   % avg
+	print "          %.1f s (max)\n" % max(cpu_times)
