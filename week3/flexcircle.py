ab95fbcc1fb7ebd91dc311431eb62dd84a70274d
Håkon V. Treider <haakonvt@gmail.com>
2015-09-15 20:38:13 +0200

    FlexCircle working

--- week3/flexcircle.py
+++ week3/flexcircle.py
@@ -1,4 +1,4 @@
-class FlexCircle:
+class FlexCircle(object):
     "Define a circle with only a given radius"
 
     def __init__(self,radius):
@@ -8,11 +8,11 @@ class FlexCircle:
 
     @property
     def area(self):
-        return self.radius**2*self.pi
+		return self.radius**2*self.pi
     @area.setter
     def area(self, area):
-        self.radius     = (area/self.pi)**(0.5)
-        self._perimeter = area*2.0/self.radius
+		self.radius     = (area/self.pi)**(0.5)
+		self._perimeter = area*2.0/self.radius
 
     @property
     def perimeter(self):
@@ -21,11 +21,16 @@ class FlexCircle:
     @perimeter.setter
     def perimeter(self, new_perimeter):
         self.radius = new_perimeter/(2.0*self.pi)
-        self._area  = pi*self.radius*self.radius
+        self._area  = self.pi*self.radius*self.radius
 
 
 if __name__ == "__main__":
     c = FlexCircle(5)
+    print "Initiating a FlexCircle object with radius = 5"
     print "Area, perimeter = ", c.area, ",", c.perimeter
-    c.area = 50
+    print "Now setting perimeter to 50.."
+    c.perimeter = 50
+    print "Area, perimeter = ", c.area, ",", c.perimeter
+    print "Now setting area to 24.."
+    c.area = 24
     print "Area, perimeter = ", c.area, ",", c.perimeter
