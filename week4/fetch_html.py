ca35aa66d6144771a722a5a2ad2b90e2fe1ef257
haakonvt <haakonvt@gmail.com>
2015-09-28 19:23:28 +0200

    meh..

--- week4/fetch_html.py
+++ week4/fetch_html.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import urllib
-import codecs
 
 def get_html_content(url):
-    #response = urllib.urlopen(url)
-    #response = codecs.open('short_noreg.txt','r','utf-8') #Speed things up BEFORE delivery...
-    response = open('short_noreg.txt', 'r')
-    return response#.read()
-
-if __name__ == '__main__':
-    url = 'http://folk.uio.no/haakonvt/'
-    data = get_html_content(url)
-    print data
+    response = urllib.urlopen(url)
+    html     = response.read()
+    return html
